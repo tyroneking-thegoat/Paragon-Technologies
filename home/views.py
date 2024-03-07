b02a4129a58cd8a7a75f73c81e30ad2958b2e7c3
will-robertson0 <wj.robertson24@gmail.com>
2024-03-07 03:41:01 -0600

    new pdf thumbnail model, begining work on scalable grid for docs on the home page

--- home/views.py
+++ home/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from .models import Thumbnail
 
 # Create your views here.
 from django.views.generic import TemplateView
 
 def home(request):
-    
-    return render(request, "course/home.html")
+    obj=Thumbnail.objects.all()
+    return render(request, "course/home.html", {'thumbnail':obj})
+
