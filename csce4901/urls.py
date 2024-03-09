ba2aee08f6c29c2dbc73243a82d82bdaea703ab2
will-robertson0 <wj.robertson24@gmail.com>
2024-03-09 02:45:37 -0600

    fixes to new model, new 'media' and 'static' directories, should support images now

--- csce4901/urls.py
+++ csce4901/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,3 +27,5 @@ urlpatterns = [
     path('upload/', include('upload.urls')),
     path("account/", include("account.urls")),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
