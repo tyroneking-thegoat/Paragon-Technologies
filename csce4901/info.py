b9f8097aa3f25fbadacf6850347c51fe4b9f0d55
mauro1100 <mauroalvarenga@my.unt.edu>
2024-03-09 14:49:08 -0600

    fixed email verification sending
    using free sendgrid smpt email sending service

--- csce4901/info.py
+++ csce4901/info.py
@@ -1,5 +1,5 @@
 EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'notetakertool@gmail.com'
-EMAIL_HOST_PASSWORD = 'pjyw fjmw pbaz dolf'
+EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_HOST_USER = 'apikey'
+EMAIL_HOST_PASSWORD = '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'
 EMAIL_PORT = 587
