246fc7e6ebb64de49b29882bf222eab5dd92f26a
mauro1100 <mauroalvarenga@my.unt.edu>
2024-03-09 15:01:15 -0600

    final email verification update

--- csce4901/info.py
+++ csce4901/info.py
@@ -1,5 +1,5 @@
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.sendgrid.net'
 EMAIL_HOST_USER = 'apikey'
-EMAIL_HOST_PASSWORD = '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'
+EMAIL_HOST_PASSWORD = 'paste sendgrid api here' #ask if you dont have it
 EMAIL_PORT = 587
