8a2f731a3f39a19c5c5c1c643c9780cf22695b61
DavidOk14 <114937803+DavidOk14@users.noreply.github.com>
2024-03-09 17:53:23 -0600

    Enroll page now verifies and responds if an enrollment code exist, TODO: Create a method to add the enrollment code to the user's information and make those courses visible under 'courses' section of the webpage

--- enroll/views.py
+++
@@ -1,4 +0,0 @@
-from django.shortcuts import render
-
-def enroll_view(request):
-    return render(request, 'enroll/enroll.html')
--- enrollment_codes/views.py
+++ enrollment_codes/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from .models import EnrollmentCode
 
-# Create your views here.
+def check_enrollment(request):
+    # Extract the enrollment code from the request's query parameters
+    enrollment_code = request.GET.get('code', None)
+
+    # Query the database to check if the enrollment code exists
+    if enrollment_code:
+        enrollment_exists = EnrollmentCode.objects.filter(EnrollCode=enrollment_code).exists()
+        if enrollment_exists:
+            return JsonResponse({'message': f'Enrollment code {enrollment_code} exists'}, status=200)
+        else:
+            return JsonResponse({'message': f'Enrollment code {enrollment_code} does not exist'}, status=404)
+    else:
+        return JsonResponse({'message': 'Enrollment code not provided'}, status=400)
+
+def enroll(request):
+    return render(request, 'enroll/enroll.html')
