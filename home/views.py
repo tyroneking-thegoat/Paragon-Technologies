69fa9617cb7fcd5063a7404940b60abb14616e95
will-robertson0 <wj.robertson24@gmail.com>
2024-04-23 19:46:03 -0500

    created home-pdfs branch to view pdfs from cards on home page. phasing out thumbnail model

--- home/views.py
+++ home/views.py
@@ -1,11 +1,27 @@
 from django.shortcuts import render
 from .models import Thumbnail
+from upload.models import PDFDocument, PDFFile
 
 # Create your views here.
 # from django.views.generic import TemplateView
 
 
 def home(request):
-    obj=Thumbnail.objects.all()
-    return render(request, 'course/home.html', {'thumbnail':obj})
+    # obj=Thumbnail.objects.all()
+    # return render(request, 'course/home.html', {'thumbnail':obj})
 
+    # obj=PDFFile.objects.all()
+    # return render(request, 'course/home.html', {'thumbnail':obj})
+
+
+    uploads = PDFFile.objects.all().select_related('pdf_document')
+    
+    documents_with_files = {}
+    for upload in uploads:
+        document_name = upload.pdf_document
+        if document_name not in documents_with_files:
+            documents_with_files[document_name] = [upload]
+        else:
+            documents_with_files[document_name].append(upload)
+
+    return render(request, 'course/home.html', {'documents_with_files': documents_with_files})
